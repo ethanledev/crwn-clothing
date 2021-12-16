795f6107c4bd4c80e6d0ddd21d64e5b7362fcfd7
Hieu Le <hieuhmle@gmail.com>
2021-12-15 23:00:41 -0600

    convert scss to styled-component for SignUp

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./sign-up.styles.scss";
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { SignUpContainer, Title } from "./sign-up.styles";
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -52,8 +52,8 @@ class SignUp extends React.Component {
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
-      <div className="sign-up">
-        <h2 className="title">I do not have an account</h2>
+      <SignUpContainer>
+        <Title>I do not have an account</Title>
         <span>Sign up with your email and password</span>
         <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
@@ -90,7 +90,7 @@ class SignUp extends React.Component {
           />
           <CustomButton type="submit">SIGN UP</CustomButton>
         </form>
-      </div>
+      </SignUpContainer>
     );
   }
 }
