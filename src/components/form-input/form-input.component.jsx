636b9a8b60a9dd8d8609501c098047edb1041798
Hieu Le <hieuhmle@gmail.com>
2021-12-15 23:40:19 -0600

    convert scss to styled-component for FormInput

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import "./form-input.styles.scss";
+import {
+  Group,
+  FormInputLabel,
+  ShrinkLabel,
+  FormInputContainer,
+} from "./form-input.styles";
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="group">
-    <input className="form-input" onChange={handleChange} {...otherProps} />
+  <Group>
+    <FormInputContainer onChange={handleChange} {...otherProps} />
     {label ? (
-      <label
-        className={`${
-          otherProps.value.length ? "shrink" : ""
-        } form-input-label`}
-      >
-        {label}
-      </label>
+      otherProps.value.length ? (
+        <ShrinkLabel>{label}</ShrinkLabel>
+      ) : (
+        <FormInputLabel>{label}</FormInputLabel>
+      )
     ) : null}
-  </div>
+  </Group>
 );
 
 export default FormInput;
