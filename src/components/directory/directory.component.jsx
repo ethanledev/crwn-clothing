23a3342c3aea8a1232388c5ed390db4f0978dc16
Hieu Le <hieuhmle@gmail.com>
2021-12-15 22:50:56 -0600

    conver scss to styled-component for Directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,14 +3,14 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 import MenuItem from "../menu-item/menu-item.component";
-import "./directory.styles.scss";
+import { DirectoryMenu } from "./directory.styles";
 
 const Directory = ({ sections }) => (
-  <div className="directory-menu">
+  <DirectoryMenu>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </div>
+  </DirectoryMenu>
 );
 
 const mapStateToProps = createStructuredSelector({
