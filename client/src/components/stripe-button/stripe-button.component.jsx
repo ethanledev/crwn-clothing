547db994b3f07921ecd041f01a76ce013c022b39
Hieu Le <hieuhmle@gmail.com>
2021-12-30 22:07:23 -0600

    clear cart after checking out successfully

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
+import { clearCart } from "../../redux/cart/cart.actions";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const StripeCheckoutButton = ({ price }) => {
+  const [checkOutSuccess, setCheckoutSuccess] = useState(false);
+  const dispatch = useDispatch();
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51JXxLUGgZjV4jYKFcFdPhfcgG3tUl2zTDtEgraELAxHrJz4edHitcuh8eXnV3oYnHcyKLVRmdzHKHMLOjqzNvzmP00xJGFdwAu";
+
+  useEffect(() => {
+    console.log("checkout success");
+    if (checkOutSuccess) {
+      dispatch(clearCart());
+      setCheckoutSuccess(false);
+    }
+  }, [checkOutSuccess, dispatch]);
 
   const onToken = (token) => {
     axios({
@@ -17,6 +30,7 @@ const StripeCheckoutButton = ({ price }) => {
       },
     })
       .then((response) => {
+        setCheckoutSuccess(true);
         alert("Payment successful");
       })
       .catch((error) => {
